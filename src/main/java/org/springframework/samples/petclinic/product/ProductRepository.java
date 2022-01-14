c02eb4473773c6fb6d40daf2e5e5a7299c55ad1b
Paula <paulaferreirajimenez@gmail.com>
2022-01-14 12:43:29 +0100

    Control Check Simulacro

--- src/main/java/org/springframework/samples/petclinic/product/ProductRepository.java
+++ src/main/java/org/springframework/samples/petclinic/product/ProductRepository.java
@@ -3,14 +3,30 @@ package org.springframework.samples.petclinic.product;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 
-
-public interface ProductRepository {
-    List<Product> findAll();
+@Repository
+public interface ProductRepository extends CrudRepository<Product, Integer>{
+    
+	List<Product> findAll();
+    
+	@Query("SELECT p FROM ProductType p")
     List<ProductType> findAllProductTypes();
-    Optional<Product> findById(int id);
-    Product findByName(String name);
-    Product save(Product p);
+    
+	Optional<Product> findById(int id);
+    
+	Product findByName(String name);
+    
+	Product save(Product p);
+	
+	@Query("SELECT p FROM ProductType p where p.name=:name")
+	ProductType getProductType (@Param("name")String name);
+	
+	@Query("SELECT p FROM Product p WHERE p.price<:price")
+	List<Product> findByPriceLessThan(@Param("price") Double price);
 }
+
--- src/main/java/org/springframework/samples/petclinic/product/ProductTypeFormatter.java
+++ src/main/java/org/springframework/samples/petclinic/product/ProductTypeFormatter.java
@@ -3,22 +3,31 @@ package org.springframework.samples.petclinic.product;
 import java.text.ParseException;
 import java.util.Locale;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.Formatter;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ProductTypeFormatter implements Formatter<ProductType>{
 
+	@Autowired
+	private ProductService productService;
     @Override
+    
     public String print(ProductType object, Locale locale) {
-        // TODO Auto-generated method stub
-        return null;
+        
+        return object.getName();
     }
 
     @Override
     public ProductType parse(String text, Locale locale) throws ParseException {
-        // TODO Auto-generated method stub
-        return null;
+        ProductType p = this.productService.getProductType(text);
+        if(p==null) {
+        	throw new ParseException(text, 0);
+        }else {
+        	return p;
+        }
+     
     }
     
 }
